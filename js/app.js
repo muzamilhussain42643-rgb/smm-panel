1e22a4d2febe5e7ec62b43fa821e004ade363514
muzamilhussain42643-rgb <muzamilhussain42643@gmail.com>
2026-07-25 03:58:03 -0700

    window.location.href = "services.html";

---
+++ js/app.js
@@ -0,0 +1,26 @@
+const supabaseUrl = "https://czkopmjmiksqmyqfuayz.supabase.co";
+const supabaseKey = "sb_publishable_2aXFNtKT0HdkMy26WiJ8Zg_OrAEmJAR";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
+
+console.log("Supabase Connected");
+
+document.getElementById("loginBtn")?.addEventListener("click", async () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    alert("Login Failed: " + error.message);
+  } else {
+    alert("Login Successful!");
+    window.location.href = "services.html";
+  }
+});
